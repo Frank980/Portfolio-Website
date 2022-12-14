ab4b2abce89937701a14542f28173d0ca274f102
Frank980 <105901796+Frank980@users.noreply.github.com>
2022-12-14 22:00:07 +0530

    Add files via upload

---
+++ src/components/Skills.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Skills() {
+  return (
+   <>
+   <div className='bg-zinc-800 text-slate-100 h-screen'>
+     <div className='w-3/5 m-auto pt-16 mb-2 p-4'>
+        <h1 className='font-bold text-3xl text-cyan-400 font-Import2'>Skills I possess</h1>
+        <ul>
+        <p className='font-semibold text-2xl py-2 text-cyan-500 font-Import2'>Tech skills</p>
+            <li className='py-1 list-disc ml-8  text-lg font-Montserrat'> <b>Github and Git</b>  : Its is a version control system used for keeping track of the versions and changes made by a single user or many users</li>
+            <li className='py-1 list-disc ml-8 text-lg font-Montserrat'> <b>C</b> : It is a Computer Programming language and one of the most recommended languages for beginners in Programming, But when mastered it is an ultimate..</li>
+            <li className='py-1 list-disc ml-8  text-lg font-Montserrat'> <b>Github and Git</b>  : Its is a version control system used for keeping track of the versions and changes made by a single user or many users</li>
+            <li className='py-1 list-disc ml-8  text-lg font-Montserrat'> <b>Github and Git</b>  : Its is a version control system used for keeping track of the versions and changes made by a single user or many users</li>
+            <li className='py-1 list-disc ml-8  text-lg font-Montserrat'> <b>Github and Git</b>  : Its is a version control system used for keeping track of the versions and changes made by a single user or many users</li>
+        
+        <p className='font-semibold text-2xl py-2 text-cyan-500 font-Import2' >Creative/Refreshing skills</p>
+            <li className='py-1 list-disc ml-8 text-lg font-Montserrat'>
+            Swimming
+            </li>
+            <li className='py-1 list-disc ml-8 text-lg font-Montserrat'>
+            Sculpture
+            </li>
+            <li className='py-1 list-disc ml-8 text-lg font-Montserrat'>
+            Swimming
+            </li>
+            <li className='py-1 list-disc ml-8 text-lg font-Montserrat'>
+            Swimming
+            </li>
+            <li className='py-1 list-disc ml-8 text-lg font-Montserrat'>
+            Swimming
+            </li>
+        </ul>
+    </div>
+    </div>
+   </>
+  )
+}
